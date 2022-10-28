a3ebf3e19b50db8e6c7d4e8a2f2621a474d57250
Alejandro Pardo <alejandro.pardo.r@gmail.com>
2022-10-28 18:20:40 +0200

    feat(events): Adds initial model for events.
    Added initial files for events, the controller, model, migration and test file

--- events-api/config/routes.rb
+++ events-api/config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'events/show'
+  get 'events/new'
+  get 'events/edit'
+  get 'events/delete'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
